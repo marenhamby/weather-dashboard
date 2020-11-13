2f9269137232de089d1c055798b3a38855c2dbd7
marenh <72942304+marenhamby@users.noreply.github.com>
2020-11-13 16:46:43 -0500

    added functions and id's to have the button add the city entered into a list on the left of the screen

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 //make sure the page has loaded before doing anything
 $(document).ready(function() {
     
+
+
+
     //when the button is clicked to submit the entered city, lots of stuff needs to happen
     $("#button-addon2").on("click", function(event){
         event.preventDefault();
         
         console.log("clicked")
         
-        //add text to list on the left side for future reference
-        var newCity = $("<a>")
-        newCity
-
-
-
+        //add variables for the new text and for the newtag which will be used to save the city on the page
+        var newCity = $("#entry").val();
+        var newTag = $("<a>");
+        //add text and class attributes
+        newTag.text(newCity)
+        newTag.attr("class", "list-group-item list-group-item-action")
+        //prepend city to the list
+        $("#cityList").prepend(newTag)
+        $("#entry").val("")
 
     })
 
