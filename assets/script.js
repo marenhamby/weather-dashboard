48533d7ac22713c97fab7301346b44bcc5ecedcd
marenh <72942304+marenhamby@users.noreply.github.com>
2020-11-13 10:05:56 -0500

    added type to the submit button and started the js file with a function to check that the page has loaded before running anything and with a click function if the submit button is clicked

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+//make sure the page has loaded before doing anything
+$(document).ready(function () {
+
+    //when the button is clicked to submit the entered city, lots of stuff needs to happen
+    $("#button-addon2").on("click", function(){
+
+
+        
+    })
+
+
+
+
+
+
+
+
+
+
+}
