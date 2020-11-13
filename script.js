3e2b4734f02e2da81c9701e146890ae80c3980e3
marenh <72942304+marenhamby@users.noreply.github.com>
2020-11-13 17:05:38 -0500

    added array to save city list and changed code to instead save to list before rendering list on screen

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
 //make sure the page has loaded before doing anything
 $(document).ready(function() {
     
+    //create an array to store the list of cities that have been searched for so they can render on the left of the screen
+    var citySearched = []
 
+    //create a function to display the list of cities on the left of the screen
+    function renderCityList () {
+        console.log(citySearched)
+        //delete the content inside of the city searched array to prevent multiplying the list
+        $("#citylist").empty()
 
+        //create for loop to loop through cities and add them to the list shown on the page
+        for (var i=0; i < citySearched.length; i++) {
+
+            //add new tag for each city and add it to the city list
+            var newTag = $("<a>");
+            newTag.text(citySearched[i]);
+            newTag.attr("class", "list-group-item list-group-item-action");
+            $("#cityList").prepend(newTag);
+        }
+    }
 
     //when the button is clicked to submit the entered city, lots of stuff needs to happen
     $("#button-addon2").on("click", function(event){
         event.preventDefault();
         
-        console.log("clicked")
-        
-        //add variables for the new text and for the newtag which will be used to save the city on the page
+        //add variable to grab the new text that was entered
         var newCity = $("#entry").val();
-        var newTag = $("<a>");
-        //add text and class attributes
-        newTag.text(newCity)
-        newTag.attr("class", "list-group-item list-group-item-action")
-        //prepend city to the list
-        $("#cityList").prepend(newTag)
+
+        //add the text to the array of cities
+        citySearched.push(newCity);
+       
+        //clear the input box to prepare for next thing to be typed
         $("#entry").val("")
 
+        //call the function to render the cities on the screen
+        renderCityList()
     })
 
 
