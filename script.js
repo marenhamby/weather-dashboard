1d2b2d3c75aece7d54f63e14fc7b239d6e0a58c8
marenh <72942304+marenhamby@users.noreply.github.com>
2020-11-13 20:32:53 -0500

    pulled current city, weather icon, temp, humidity, and wind speed from api call and displayed on screen. added variables to handle temperature and wind speed calculations. added variable to put together the icon url. moved the current weather function out of the click function and changed newCity to be a global variable

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
     //create variable for api key to be used throughout the document
     var key = "923dd5734cd0c672e44b457634e8ad6c"
 
+    //create variable to put the current city info in
+    var newCity = ""
 
     //create a function to display the list of cities on the left of the screen
     function renderCityList () {
@@ -25,6 +27,31 @@ $(document).ready(function() {
         }
     }
 
+    //create function to pull current weather information
+    function currentWeather() {
+
+        //create variable for the api url for the current weather
+        var currentQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + newCity + "&appid=" + key
+
+        //make api call for current weather
+        $.ajax({
+            url: currentQueryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response)
+            //convert temperature to fahrenheit from kelvin, wind from meter/sec to mph, and adding link for icon
+            var fTemp = Math.floor((response.main.temp - 273.15) * 1.8 + 32);
+            var windSpeed = Math.floor(response.wind.speed * 2.237);
+            var weatherIcon = "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png"
+            //save input to the current weather locations on the page
+            $("#currentCityDate").text(response.name);
+            $("#currentIcon").attr("src", weatherIcon);
+            $("#currentTemp").text("Current temperature: " + fTemp + String.fromCharCode(176) + "F");
+            $("#currentHumid").text("Current humidity: " + response.main.humidity + "%");
+            $("#currentSpeed").text("Current wind speed: " + windSpeed + "mph");
+        });
+
+    };
     
     //create function to pull forecast information
     
@@ -33,8 +60,8 @@ $(document).ready(function() {
     $("#button-addon2").on("click", function(event){
         event.preventDefault();
         
-        //add variable to grab the new text that was entered
-        var newCity = $("#entry").val();
+        //add value to variable for new city from the new text that was entered
+        newCity = $("#entry").val();
         
         //add the text to the array of cities
         citySearched.push(newCity);
@@ -45,23 +72,6 @@ $(document).ready(function() {
         //call the function to render the cities on the screen
         renderCityList()
         
-        //create function to pull current weather information
-        function currentWeather() {
-    
-            //create variable for the api url for the current weather
-            var currentQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + newCity + "&appid=" + key
-    
-            //make api call
-            $.ajax({
-                url: currentQueryURL,
-                method: "GET"
-            }).then(function(response) {
-                console.log(response)
-
-
-            });
-
-        };
 
         //call function that pulls current weather
         currentWeather();
